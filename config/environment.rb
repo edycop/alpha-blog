d20570da60f51beccaa3a861b38c6298f0af47af
Edwin Caldon <edycop@gmail.com>
2022-07-25 16:37:02 -0500

    add custom css for error messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configuration to avoid html tags for errors in the view pages
+ActionView::Base.field_error_proc = Proc.new do |html_tag, _instance|
+  html_tag.html_safe
+end
